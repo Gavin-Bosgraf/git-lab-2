b57df31668864a27ac6a50ad1befc1244c90e2af
Gavin <gb856421@ohio.edu>
2023-09-13 16:59:13 -0400

    Done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gavin Bosgraf
+ *        Date: 9/8/23
+ * 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
